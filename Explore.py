a22a8300d03f7ad00a422892834889790fa102bc
David J. Desjardins <david.desjardins@defisafety.com>
2022-10-19 10:00:00 -0400

    pls dont break my computer

--- Explore.py
+++ Explore.py
@@ -2,7 +2,10 @@
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
+from autoviz.AutoViz_Class import AutoViz_Class
+import os
 
+#set and sort dataframe
 df=pd.read_csv('DATA.csv',sep=',')
 df["DATE"] = pd.to_datetime(df["DATE"])
 df=df.sort_values(by=["DATE"],ascending=True)
@@ -14,7 +17,18 @@ def MatrixHeatmap():
     sn.heatmap(corr_matrix, annot=True)
     plt.show()
 
+## Time plot of var unemployment
+def UnemploymentPlot():
+    plt.plot(df["DATE"],df['UNRATE(%)'])
+    #plt.xticks(fontsize=5,rotation=90)
+    plt.show()
 
-plt.plot(df["DATE"],df['UNRATE(%)'])
-#plt.xticks(fontsize=5,rotation=90)
-plt.show()
+#visualization
+def AutoVizTest(ForceGen):
+    if os.path.exists('./Visuals')==False or ForceGen==True:
+        AV = AutoViz_Class()
+        AV.AutoViz(filename='',dfte=df,verbose=1,chart_format='html',depVar='UNRATE(%)',save_plot_dir='./Visuals')
+    else:
+        print('Visuals already exist, and it takes a while to generate them. You can coerce this function by passing in True as the first positional argument')
+
+AutoVizTest(False)
